2b61b9ae27aee89568d6d6a512f0d942e92c3de2
Ashanti Williams <ashanti.williams.16@tfacorps.org>
2018-07-11 14:39:38 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -16,7 +16,7 @@ end
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 rainbow_colors = []
-rainbow_colors[3] =  "green"
-rainbow_colors[4] = "blue"
+rainbow_colors << "green"
+rainbow_colors << "blue"
 return rainbow_colors
 end
